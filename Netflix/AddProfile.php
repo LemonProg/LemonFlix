3230661ffffb6f65d34d59c3755934f4b48a66e7
LEMON シ <ttroule32@gmail.com>
2022-09-24 17:49:42 +0200

    commit

--- Netflix/AddProfile.php
+++ Netflix/AddProfile.php
@@ -40,7 +40,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Netflix - AddProfile Page</title>
-    <link rel="stylesheet" href="styleAddProfils.css">
+    <link rel="stylesheet" href="styleAddProfile.css">
     <link rel="icon" type="image/pngn" href="../img/favicon.png">
 </head>
 <body>
