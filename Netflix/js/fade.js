e24f4e23f4f90e450edf7e31153947ace9547b72
LEMON シ <87497636+LemonProg@users.noreply.github.com>
2022-12-03 01:05:55 +0100

    Del

--- Netflix/js/fade.js
+++
@@ -1,6 +0,0 @@
-let form = document.querySelector('#anime_form');
-
-form.addEventListener("submit", () => {
-    document.body.style.animationName = "fade";
-    document.body.style.animationDuration = "1s";
-});
