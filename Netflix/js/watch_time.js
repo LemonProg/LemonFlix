b6e9b5d193e57d1bd7dc7c01ae7503ae4c57b54a
LEMON シ <87497636+LemonProg@users.noreply.github.com>
2022-12-05 21:21:52 +0100

    Delete Netflix directory

--- Netflix/js/watch_time.js
+++
@@ -1,7 +0,0 @@
-focus();
-const listener = window.addEventListener('blur', () => {
-  if (document.activeElement === document.querySelector('#player')) {
-    console.log('clicked on iframe')
-  }
-  window.removeEventListener('blur', listener);
-});
